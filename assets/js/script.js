ae0f7d63562599f61af586bd7b8fb08f5a13ffd8
jrgrbst <j.rubiosoto@gmail.com>
2020-10-07 16:10:50 -0300

    Jorge Rubio Soto - Portafolio 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,10 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
 
+$(document).ready(function(){
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+});
